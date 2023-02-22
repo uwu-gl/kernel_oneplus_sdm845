2a4f75506616b90d1c9bfdebba0691597c12d825
Gergo Koteles <soyer@irl.hu>
2023-02-22 01:10:33 +0100

    Input: add ABS_SND_PROFILE
    ABS_SND_PROFILE used to describe the state of a multi-value sound profile
    switch. This will be used for the alert-slider on OnePlus phones or other
    phones.
    
    Profile values added as SND_PROFLE_(SILENT|VIBRATE|RING) identifiers
    to input-event-codes.h so they can be used from DTS.
    
    Signed-off-by: Gergo Koteles <soyer@irl.hu>

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -867,6 +867,7 @@
 
 #define ABS_VOLUME		0x20
 #define ABS_PROFILE		0x21
+#define ABS_SND_PROFILE		0x22
 
 #define ABS_MISC		0x28
 
@@ -974,5 +975,13 @@
 #define SND_TONE		0x02
 #define SND_MAX			0x07
 #define SND_CNT			(SND_MAX+1)
+
+/*
+ * ABS_SND_PROFILE values
+ */
+
+#define SND_PROFILE_SILENT	0x00
+#define SND_PROFILE_VIBRATE	0x01
+#define SND_PROFILE_RING	0x02
 
 #endif
