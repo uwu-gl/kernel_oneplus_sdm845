d0732abb42b4bfc7360d7384a7eaa929cd4a5524
Grill-Laux <137363994+Grill-Laux@users.noreply.github.com>
2024-02-06 22:26:51 +0800

    build_sh: Start to use dtc

--- build_kernel_github_action.sh
+++ build_kernel_github_action.sh
@@ -10,6 +10,8 @@ export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 # export CONFIG_BUILD_ARM64_DT_OVERLAY=y
+DTC_EXT=~/toolchains/dtc/dtc
+export DTC_EXT=~/toolchains/dtc/dtc
 
 echo "[ i ]  Setting up ARCH..."
 export ARCH=arm64
