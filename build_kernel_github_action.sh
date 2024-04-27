045ece16f0e442d9fdd47ec091d0c7af57aa2ab1
gluwu <137363994+Grill-Laux@users.noreply.github.com>
2024-04-27 15:34:32 +0800

    build_sh: Update dtc

--- build_kernel_github_action.sh
+++ build_kernel_github_action.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "[ i ]  Parparing Environment..."
-cd $GITHUB_WORKSPACE/android-kernel
-echo "[ i ]  Setting up PATH..."
+echo "### Setting up PATH..."
 export PATH=~/toolchains/clang/bin/:$PATH
 export CC=clang
 CLANG=~/toolchains/clang/bin
@@ -14,9 +12,9 @@ export ARCH=arm64
 export CLANG_TRIPLE=aarch64-linux-gnu
 export CROSS_COMPILE=aarch64-linux-android-
 export CROSS_COMPILE_ARM32=arm-linux-androideabi-
-chmod +x $GITHUB_WORKSPACE/android-kernel/dtc/dtc
-export DTC_EXT=$GITHUB_WORKSPACE/android-kernel/dtc/dtc
-echo "[ i ]  Setting up ARCH..."
+chmod +x dtc/dtc
+export DTC_EXT=dtc/dtc
+echo "### Setting up ARCH..."
 export ARCH=arm64
 export SUBARCH=arm64
 
@@ -24,21 +22,21 @@ if [ ! -d "out" ]; then
 	mkdir out
 fi
 
-echo "[ i ]  Making kernel config..."
+echo "### Making kernel config..."
 make ARCH=arm64 O=out CC="ccache clang" sdm845-perf_defconfig
 # make ARCH=arm64 O=out CC=clang oldconfig
 
-echo "[ i ]  Making kernel...(This may take long time)"
+echo "### Making kernel...(This may take long time)"
 make ARCH=arm64 O=out CC=clang -j$(nproc --all) 2>&1 | tee kernel_build_log.txt
 
 
 if [ -f out/arch/arm64/boot/Image.gz-dtb ]; then
-	echo "[ i ]  Finished kernel building!"
-	echo "[ i ]  Exiting script..."
+	echo "### Finished kernel building!"
+	echo "### Exiting script..."
 	exit 0
 else
 	echo " "
-	echo "[ x ]  Kernel building failed!"
-        echo "[ x ]  Exiting script..."
+	echo "### Kernel building failed!"
+        echo "### Exiting script..."
 	exit 0
 fi
