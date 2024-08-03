LineageOS/android_kernel_oneplus_sdm845

drivers: qcom: Import op_rf_cable_monitor
Change-Id: I52789ebad63d190d95cb03b94b619846bfa8ab80

drivers/soc/qcom/subsystem_restart.c
@@ -813,6 +813,22 @@ static struct subsys_device *find_subsys(const char *str)
 	return dev ? to_subsys(dev) : NULL;
 }
 
+static int restart_level;/*system original val*/
+int op_restart_modem(void)
+{
+	struct subsys_device *subsys = find_subsys("modem");
+
+	if (!subsys)
+		return -ENODEV;
+	restart_level = subsys->restart_level;
+	subsys->restart_level = RESET_SUBSYS_COUPLED;
+	if (subsystem_restart("modem") == -ENODEV)
+		pr_err("%s: SSR call failed\n", __func__);
+	subsys->restart_level = restart_level;
+	return 0;
+}
+EXPORT_SYMBOL(op_restart_modem);
+
 static int subsys_start(struct subsys_device *subsys)
 {
 	int ret;

uwu-gl/kernel_oneplus_sdm845

drivers/soc/qcom/subsystem_restart.c
@@ -911,29 +911,21 @@ struct subsys_device *find_subsys_device(const char *str)
 }
 EXPORT_SYMBOL(find_subsys_device);
 
-#ifdef OPLUS_BUG_STABILITY
-int op_restart_modem(struct subsys_device *subsys)
+static int restart_level;/*system original val*/
+int op_restart_modem(void)
 {
-	int restart_level;
+	struct subsys_device *subsys = find_subsys_device("modem");
 
-	if (!subsys) {
+	if (!subsys)
 		return -ENODEV;
-	}
-
-	pr_err("%s\n", __func__);
-
 	restart_level = subsys->restart_level;
 	subsys->restart_level = RESET_SUBSYS_COUPLED;
-	if (subsys->desc->force_reset) {
-		subsys->desc->force_reset(subsys->desc);
-	}
-
+	if (subsystem_restart("modem") == -ENODEV)
+		pr_err("%s: SSR call failed\n", __func__);
 	subsys->restart_level = restart_level;
-
 	return 0;
 }
 EXPORT_SYMBOL(op_restart_modem);
-#endif /* OPLUS_BUG_STABILITY */
 
 static int subsys_start(struct subsys_device *subsys)
 {