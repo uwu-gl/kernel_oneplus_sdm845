5e2ee2c87ed9aef120c4b13bf3f917108935ff8d
uwu-gl <moegluwu0@gmail.com>
2025-01-28 18:26:57 +0800

    fs: Deduplicate code

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2331,7 +2331,6 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 	if (new_tsec->sid == old_tsec->sid)
 		return 0; /* No change in credentials */
 
-<<<<<<< HEAD
 	if (!ksu_sid)
 		security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);
 
